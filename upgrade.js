cb5fc576b019bc64887bd1575bd3e157e440dbb3
Aristeidis Bampakos <bampakoa@gmail.com>
2022-07-09 15:43:44 +0300

    Add translations migration in the upgrade script

--- upgrade.js
+++ upgrade.js
@@ -4,6 +4,8 @@ const { exec } = require('child_process');
 
 const angularGrPath = path.resolve('.');
 const angularPath = path.resolve('..', 'angular');
+const angularIoPath = path.resolve('..', 'angular', 'aio');
+const translationsPath = path.resolve('..', 'aio-translations');
 const angularVersion = process.argv.slice(2);
 
 if (angularVersion.length === 0) {
@@ -24,7 +26,23 @@ exec(`git fetch --all && git checkout ${angularVersion}`);
 fs.readdir(angularPath, (_, entries) => {
   for(entry of entries) {
     if (!['.git', '.github', '.circleci', '.devcontainer', '.husky', '.ng-dev', '.pullapprove.yml', '.gitmessage'].includes(entry)) {
-      fs.copy(entry, path.join(angularGrPath, entry));
+      fs.copy(path.join(angularPath, entry), path.join(angularGrPath, entry));
+    }
+  }
+});
+
+fs.readdir(translationsPath, (_, entries) => {
+  for(entry of entries) {
+    if (!['.git', '.gitignore'].includes(entry)) {
+      fs.rmSync(path.join(translationsPath, entry), { recursive: true, force: true });
+    }
+  }
+});
+
+fs.readdir(angularIoPath, (_, entries) => {
+  for(entry of entries) {
+    if (!['.git', '.gitignore'].includes(entry)) {
+      fs.copy(path.join(angularIoPath, entry), path.join(translationsPath, entry));
     }
   }
 });
